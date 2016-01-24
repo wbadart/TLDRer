6e38125e7b39f81a82b18c785c050f8d4fda8571
piercecunneen <pcunneen19@gmail.com>
2016-01-24 15:10:34 -0500

    added #define variables, added dollar sign for unix command code

--- main.c
+++ main.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+
+#define maxInputs 12
+#define maxLength 128
+
 
 int main(int argc, char **argv){
     char command[32];
     char filename[32];
     char description[256];
     int num_examples, num_resources, i;
-    char example_description[12][128];
-    char example_code[12][128];
-    char resource_name[12][128];
-    char resource_url[12][128];
+    char example_description[maxInputs][maxLength];
+    char example_code[maxInputs][maxLength];
+    char resource_name[maxInputs][maxLength];
+    char resource_url[maxInputs][maxLength];
+
+
+
+
     printf("\nWelcome to TLDRer\n\n");
     if(argc > 1){
         strncpy(command, argv[1], 32);
@@ -35,13 +44,15 @@ int main(int argc, char **argv){
 
     printf("\nHow many examples would you like to give? ");
     scanf("%d", &num_examples);
+
+
     for(i = 0; i < num_examples; i++){
         printf("Example %d description: ", i + 1);
         getchar();
-        fgets(example_description[i], 128, stdin);
+        fgets(example_description[i], maxLength, stdin);
         example_description[i][strlen(example_description[i]) - 1] = '\0';
         printf("Example %d code: ", i + 1);
-        fgets(example_code[i], 128, stdin);
+        fgets(example_code[i], maxLength, stdin);
         example_code[i][strlen(example_code[i]) - 1] = '\0';
     }
 
@@ -50,24 +61,24 @@ int main(int argc, char **argv){
     for(i = 0; i < num_resources; i++){
         printf("Resource %d name: ", i + 1);
         getchar();
-        fgets(resource_name[i], 128, stdin);
+        fgets(resource_name[i], maxLength, stdin);
         resource_name[i][strlen(resource_name[i]) - 1] = '\0';
         printf("Resouce %d url: ", i + 1);
-        fgets(resource_url[i], 128, stdin);
+        fgets(resource_url[i], maxLength, stdin);
         resource_url[i][strlen(resource_url[i]) - 1] = '\0';
     }
-    
+
     FILE *fp;
     if(fp = fopen(filename, "ab+")){
         fprintf(fp, "TLDR - %s\n=====\n\nOverview\n--------\n\n%s\n\nExamples\n--------\n\n", command, description);
         for(i = 0; i < num_examples; i++){
-            fprintf(fp, "- %s\n\n    %s\n\n", example_description[i], example_code[i]);
+            fprintf(fp, "- %s\n\n    $%s\n\n", example_description[i], example_code[i]);
         }
         fprintf(fp, "Resources\n---------\n\n");
         for(i = 0; i < num_resources; i++){
             fprintf(fp, "- [%s](%s)\n", resource_name[i], resource_url[i]);
         }
-                    
+
     }
     printf("\n\n");
     fclose(fp);
