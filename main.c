414fa99a5f95177f52893672fa386c2eae58d1f3
Will Badart <wbadart@nd.edu>
2016-01-23 19:00:02 -0500

    Fixed filename bug
    Will no longer write incoherent file when command and filename not specified from command line.  Also cleaned up the output a bit.

--- main.c
+++ main.c
@@ -10,24 +10,23 @@ int main(int argc, char **argv){
     char example_code[12][128];
     char resource_name[12][128];
     char resource_url[12][128];
+    printf("\nWelcome to TLDRer\n\n");
     if(argc > 1){
         strncpy(command, argv[1], 32);
-        command[strlen(command)] = '\0';
         printf("Using command <%s>\n", command);
         if(argc > 2){
             strncpy(filename, argv[2], 32);
-            filename[strlen(filename)] = '\0';
             printf("Using file name <%s>\n", filename);
         }
     }else{
-        printf("Command: ");
+        printf("Command name: ");
         fgets(command, 32, stdin);
-        command[strlen(command)] = '\0';
+        command[strlen(command) - 1] = '\0';
     }
     if(argc < 3){
         printf("Destination file: ");
         fgets(filename, 32, stdin);
-        filename[strlen(filename)] = '\0';
+        filename[strlen(filename) - 1] = '\0';
     }
 
     printf("\nDescription: ");
@@ -37,11 +36,11 @@ int main(int argc, char **argv){
     printf("\nHow many examples would you like to give? ");
     scanf("%d", &num_examples);
     for(i = 0; i < num_examples; i++){
-        printf("Example %d description: ", i);
+        printf("Example %d description: ", i + 1);
         getchar();
         fgets(example_description[i], 128, stdin);
         example_description[i][strlen(example_description[i]) - 1] = '\0';
-        printf("Example %d code: ", i);
+        printf("Example %d code: ", i + 1);
         fgets(example_code[i], 128, stdin);
         example_code[i][strlen(example_code[i]) - 1] = '\0';
     }
@@ -49,11 +48,11 @@ int main(int argc, char **argv){
     printf("\nHow many resources would you like to give? ");
     scanf("%d", &num_resources);
     for(i = 0; i < num_resources; i++){
-        printf("Resource %d name: ", i);
+        printf("Resource %d name: ", i + 1);
         getchar();
         fgets(resource_name[i], 128, stdin);
         resource_name[i][strlen(resource_name[i]) - 1] = '\0';
-        printf("Resouce %d url: ", i);
+        printf("Resouce %d url: ", i + 1);
         fgets(resource_url[i], 128, stdin);
         resource_url[i][strlen(resource_url[i]) - 1] = '\0';
     }
@@ -66,11 +65,11 @@ int main(int argc, char **argv){
         }
         fprintf(fp, "Resources\n---------\n\n");
         for(i = 0; i < num_resources; i++){
-            fprintf(fp, "- [%s](%s)", resource_name[i], resource_url[i]);
+            fprintf(fp, "- [%s](%s)\n", resource_name[i], resource_url[i]);
         }
                     
     }
-
+    printf("\n\n");
     fclose(fp);
 
     return 0;
